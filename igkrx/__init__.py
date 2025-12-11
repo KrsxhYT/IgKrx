a540bc3d04015df65a3324acec7e99ea8cbd9754
Krsxh <krishnaagfxs@gmail.com>
2025-12-11 12:39:28 +0530

    Create __init__.py

---
+++ igkrx/__init__.py
@@ -0,0 +1,3 @@
+from .igkrx import igresetv1, igresetv2, infoig, iguid_info, download_reel
+from .igkrx import gen_igcookie, initiate_signup, search_instagram_users
+from .igkrx import get_instagram_hashtag_info, check_username_availability
